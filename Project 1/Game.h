64e9d02e40e5d64b03e2c66c6057edd5ba198b63
Weslyn Clarke <32582459+weslynclarke@users.noreply.github.com>
2018-09-30 22:04:20 -0700

    Update Game.h

--- Project 1/Game.h
+++ Project 1/Game.h
@@ -1,2 +1,32 @@
 // Created by Weslyn Clarke
 // Copyright (c) 2018 Weslyn Clarke. All rights reserved.
+
+#ifndef GAME_INCLUDED
+#define GAME_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <random>
+#include <utility>
+#include <cstdlib>
+using namespace std;
+
+class City;
+
+class Game
+{
+public:
+	// Constructor/destructor
+	Game(int rows, int cols, int nFlatulans);
+	~Game();
+
+	// Mutators
+	void play();
+
+private:
+	City * m_city;
+};
+
+int decodeDirection(char dir);
+
+#endif // GAME_INCLUDED
