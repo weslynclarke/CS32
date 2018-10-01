c36d56b1800987346bd6bef07a0c1ea954969073
Weslyn Clarke <32582459+weslynclarke@users.noreply.github.com>
2018-09-30 22:20:56 -0700

    Update chetyris.cpp

--- Project 3/chetyris.cpp
+++ Project 3/chetyris.cpp
@@ -3,13 +3,13 @@
 
 #include "Game.h"
 
-const int WELL_WIDTH  = 10;
+const int WELL_WIDTH = 10;
 const int WELL_HEIGHT = 18;
 
 int main()
 {
-    Game g(WELL_WIDTH, WELL_HEIGHT);
-    g.play();
+	Game g(WELL_WIDTH, WELL_HEIGHT);
+	g.play();
 }
 
 #include "Piece.h"
@@ -17,8 +17,8 @@ int main()
 
 PieceType chooseRandomPieceType()
 {
-    static std::random_device aRandomDevice;
-    static std::default_random_engine generator(aRandomDevice());
-    std::uniform_int_distribution<> distro(0, NUM_PIECE_TYPES-1);
-    return static_cast<PieceType>(distro(generator));
+	static std::random_device aRandomDevice;
+	static std::default_random_engine generator(aRandomDevice());
+	std::uniform_int_distribution<> distro(0, NUM_PIECE_TYPES - 1);
+	return static_cast<PieceType>(distro(generator));
 }
