cda6a34b7f0b33d71431e46151511d209386068f
Weslyn Clarke <32582459+weslynclarke@users.noreply.github.com>
2018-09-30 22:16:38 -0700

    Update UserInterface.h

--- Project 3/UserInterface.h
+++ Project 3/UserInterface.h
@@ -1,11 +1,11 @@
-#ifndef USERINTERFACE_INCLUDED
-#define USERINTERFACE_INCLUDED
+#ifndef USERINTERFACE_INCLUDE
+#define USERINTERFACE_INCLUDE
 
 #include <string>
 
-const char ARROW_UP    = '8';
-const char ARROW_DOWN  = '2';
-const char ARROW_LEFT  = '4';
+const char ARROW_UP = '8';
+const char ARROW_DOWN = '2';
+const char ARROW_LEFT = '4';
 const char ARROW_RIGHT = '6';
 
 ///////////////////////////////////////////////////////////
@@ -16,18 +16,18 @@ class ScreenImpl;
 
 class Screen
 {
-  public:
-    Screen(int width, int height);
-    ~Screen();
-    void clear();
-    void gotoXY(int x, int y); //positions cursor at the x and y coordinates supplied as parameters, text appears starting at this point
-    void printChar(char ch); // write a single character
-    void printString(std::string s); //write a single string
-    void printStringClearLine(std::string s); //writes a string while clearing the rest of characters on current line
-    void refresh();
+public:
+	Screen(int width, int height);
+	~Screen();
+	void clear();
+	void gotoXY(int x, int y); //positions cursor at the x and y coordinates supplied as parameters, text appears starting at this point
+	void printChar(char ch); // write a single character
+	void printString(std::string s); //write a single string
+	void printStringClearLine(std::string s); //writes a string while clearing the rest of characters on current line
+	void refresh();
 
-  private:
-    ScreenImpl* m_impl;
+private:
+	ScreenImpl * m_impl;
 };
 
 ///////////////////////////////////////////////////////////
@@ -38,9 +38,9 @@ bool getCharIfAny(char& ch); // if user hits a key, return the character
 void waitForEnter(); // pauses until user presses enter key
 void discardPendingKeys(); //throws away unprocessed key presses
 
-///////////////////////////////////////////////////////////
-// Class for Timing
-///////////////////////////////////////////////////////////
+						   ///////////////////////////////////////////////////////////
+						   // Class for Timing
+						   ///////////////////////////////////////////////////////////
 
 //========================================================================
 // Timer t;                 // create and start a timer
@@ -52,23 +52,23 @@ void discardPendingKeys(); //throws away unprocessed key presses
 
 class Timer
 {
-  public:
-    Timer()
-    {
-        start();
-    }
-    void start()
-    {
-        m_time = std::chrono::high_resolution_clock::now();
-    }
-    double elapsed() const
-    {   
-        std::chrono::duration<double,std::milli> diff =
-                          std::chrono::high_resolution_clock::now() - m_time;
-        return diff.count();
-    }
-  private:
-    std::chrono::high_resolution_clock::time_point m_time;
+public:
+	Timer()
+	{
+		start();
+	}
+	void start()
+	{
+		m_time = std::chrono::high_resolution_clock::now();
+	}
+	double elapsed() const
+	{
+		std::chrono::duration<double, std::milli> diff =
+			std::chrono::high_resolution_clock::now() - m_time;
+		return diff.count();
+	}
+private:
+	std::chrono::high_resolution_clock::time_point m_time;
 };
 
-#endif // USERINTERFACE_INCLUDED
+#endif // USERINTERFACE_INCLUDE
