80a0014d96253b8143f243a28dd270e84a322087
Weslyn Clarke <32582459+weslynclarke@users.noreply.github.com>
2018-09-30 22:15:05 -0700

    Update Game.h

--- Project 3/Game.h
+++ Project 3/Game.h
@@ -1,39 +1,40 @@
 // Created by Weslyn Clarke
 // Copyright (c) 2018 Weslyn Clarke. All rights reserved.
 
-#ifndef GAME_INCLUDED
-#define GAME_INCLUDED
+#ifndef GAME_INCLUDE
+#define GAME_INCLUDE
 
 #include "Well.h"
 #include "UserInterface.h"
 #include <string>
-// [Add other #include directives as necessary.]
 
 class Game
 {
-  public:
-    Game(int width, int height);
-    void play();
-    bool playOneLevel();
-    void displayPrompt(std::string s);
-    void displayStatus();
-    // [Add other members as necessary.]
-	void drawPiece(Screen& screen, int xCoord, int yCoord, int piece, int rotation);
-	bool fall(int x, int y, int piece, int rotation, int direction);
-	bool rotateCurrentPiece(int piece, int& currentRotation);
-	int scoreFunction();
-	void refreshScreen();
-	void rowsLeftUpdate();
-	void removeFilledRows(bool& endLevel, bool& nextLevel); // deletes filled rows in the well
-	std::string toStringify(int becomeString);
-	std::string levelStr();
-	std::string scoreStr();
+public:
+	Game(int width, int height);
+	~Game();
+	void play();
+	bool playOneLevel();
+	void displayPrompt(std::string s);
+	void displayStatus();
 
-  private:
-    Well*    m_well;
-    Screen  m_screen;
-    int     m_level;
-    // [Add other members as necessary.]
+	void drawPiece(Screen& screen, int xCoord, int yCoord, Piece* piece, int rotation); // draws the piece
+	bool fall(int x, int y, Piece* piece, int rotation, int direction); // checks if the piece can fall
+	bool rotateCurrentPiece(Piece* piece, int& currentRotation); // checks if the current piece can rotate
+	int scoreFunction(); // increments the score
+	void refreshScreen(); // refreshes the screen
+	void rowsLeftUpdate(); // updates the number of rows left after each level
+	void removeFilledRows(bool& endLevel, bool& nextLevel); // deletes filled rows in the well
+	std::string toStringify(int becomeString); // turns numebrs into strings for status output
+	void updatePieces(); // sets current piece to the next piece
+	void chooseNextPiece(); // chooses the pieces
+	Piece* chooseRandomPiece(int& pieceNum);
+
+private:
+	Well* m_well;
+	Screen  m_screen;
+	int     m_level;
+
 	int fallX; // position x of the falling piece
 	int fallY; // position y of the falling piece
 	int down; // 0
@@ -41,14 +42,15 @@ class Game
 	int left; // 2
 	int same; // used for rotating a piece in place
 	int mScore; // score count
-	int currentPiece; // keep track of current piece
-	int nextPiece; // number of the next piece
+	Piece* currentPiece; // keep track of current piece
+	Piece* nextPiece; // number of the next piece
 	int mRotation; // orientation number
 	bool levelStarted; // level has started or not
 	int maxTime; // the max timer time
 	int scoreCalled; // number of times score function is called at once
 	int rowsLeft; // the rows left till next level
-	Piece* tetro; // piece pointer to access the piece functions
+	int currentPieceNum; // stores the enum number of the current piece
+	int nextPieceNum; // stores the enum number of the next piece
 };
 
 #endif // GAME_INCLUDED
