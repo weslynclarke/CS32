09e0dddaa0186a2ac8ae576dac3e3125fc8744ac
Weslyn Clarke <32582459+weslynclarke@users.noreply.github.com>
2018-09-30 22:04:58 -0700

    Update City.h

--- Project 1/City.h
+++ Project 1/City.h
@@ -1,2 +1,55 @@
 // Created by Weslyn Clarke
 // Copyright (c) 2018 Weslyn Clarke. All rights reserved.
+
+#ifndef CITY_INCLUDED
+#define CITY_INCLUDED
+
+#include "globals.h"
+#include "History.h"
+#include <iostream>
+#include <string>
+#include <random>
+#include <utility>
+#include <cstdlib>
+using namespace std;
+
+class Player;
+class Flatulan;
+
+class City
+{
+public:
+	// Constructor/destructor
+	City(int nRows, int nCols);
+	~City();
+
+	// Accessors
+	int     rows() const;
+	int     cols() const;
+	Player* player() const;
+	int     flatulanCount() const;
+	int     nFlatulansAt(int r, int c) const;
+	bool    isPlayerAt(int r, int c) const;
+	bool    determineNewPosition(int& r, int& c, int dir) const;
+	void    display() const;
+	History& history();
+
+	// Mutators
+	bool  addFlatulan(int r, int c);
+	bool  addPlayer(int r, int c);
+	void  preachToFlatulansAroundPlayer();
+	void  moveFlatulans();
+
+private:
+	int       m_rows;
+	int       m_cols;
+	Player*   m_player;
+	Flatulan* m_flatulans[MAXFLATULANS];
+	int       m_nFlatulans;
+	History	m_history;
+
+	// Helper functions
+	bool isInBounds(int r, int c) const;
+};
+
+#endif // !CITY_INCLUDED
