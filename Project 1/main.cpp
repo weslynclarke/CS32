2fb0cad23d4935b009e458fd87ea2ebc0b6c5c8a
Weslyn Clarke <32582459+weslynclarke@users.noreply.github.com>
2018-09-30 22:01:18 -0700

    Update main.cpp

--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -1,2 +1,13 @@
 // Created by Weslyn Clarke
 // Copyright (c) 2018 Weslyn Clarke. All rights reserved.
+
+#include "Game.h"
+int main()
+{
+	// Create a game
+	// Use this instead to create a mini-game:   Game g(3, 4, 2);
+	Game g(7, 8, 25);
+
+	// Play the game
+	g.play();
+}
