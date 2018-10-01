48960d297dcc18a9c35e8a277084abb0449512cd
Weslyn Clarke <32582459+weslynclarke@users.noreply.github.com>
2018-09-30 22:03:46 -0700

    Update History.h

--- Project 1/History.h
+++ Project 1/History.h
@@ -1,2 +1,21 @@
 // Created by Weslyn Clarke
 // Copyright (c) 2018 Weslyn Clarke. All rights reserved.
+
+#ifndef HISTORY_H
+#define HISTORY_H
+
+#include "globals.h"
+
+class History
+{
+public:
+	History(int nRows, int nCols);
+	bool record(int r, int c);
+	void display() const;
+private:
+	int mh_rows;
+	int mh_cols;
+	int m_historyBoard[MAXROWS][MAXCOLS];
+};
+
+#endif // !HISTORY_H
